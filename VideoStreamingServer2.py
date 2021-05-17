8215553bf873d8797daddb49c81e07f094780de0
woorim1022 <woorim1022@naver.com>
2021-05-17 19:26:11 +0900

    Add files

---
+++ VideoStreamingServer2.py
@@ -0,0 +1,21 @@
+import MyCamera
+
+def show(camera):
+    while True:
+        frame = camera.getStreaming()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route("/show")
+def showVideo():
+    return Response(show(MyCamera.MyCamera()), mimetype="multipart/x-mixed-replace;boundary=frame")
+
+
+if __name__ == "__main__":
+    try:
+        app.run(host="0.0.0.0", debug=True, threaded=True)
+
+    except KeyboardInterrupt:
+        print("종료")
+
