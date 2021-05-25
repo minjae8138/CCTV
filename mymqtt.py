fa8a2e16780c3c9dbc96eaba92ce4accf42cb7b3
woorim1022 <woorim1022@naver.com>
2021-05-25 14:49:42 +0900

    Modify MQTT

--- mymqtt.py
+++ mymqtt.py
@@ -7,28 +7,28 @@ class MyMqtt():
     def __init__(self, topic, value):
         super().__init__()
         client = mqtt.Client()
-
+        self.camera = MyCamera.MyCamera()
         client.connect("192.168.0.62", 1883, 60)
         client.on_connect = self.on_connect
         client.on_message = self.on_message
         client.loop_forever()
 
     def on_connect(self, client, userdata, flags, rc):
-        print("connect.." + str(rc))
-        if rc == 0:
-            client.subscribe("sensor")
-        else:
-            print("연결실패")
+            print("connect.." + str(rc))
+            if rc == 0:
+                client.subscribe("sensor")
+            else:
+                print("연결실패")
 
     def on_message(self, client, userdata, msg):
         myval = msg.payload.decode("utf-8")
         print(myval)
         print(msg.topic + "----" + str(myval))
-        camera = MyCamera.MyCamera()
+
         if myval == "start":
             while True:
-                frame = camera.getStreaming()
-                publish.single("web", frame, hostname="192.168.0.182")
+                frame = self.camera.getStreaming()
+                publish.single("web", frame, hostname="192.168.0.62")
         else:
             pass
 
