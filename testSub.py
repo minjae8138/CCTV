95a93d667dfaf16bebf56abc0758fe8aae82df3e
woorim1022 <woorim1022@naver.com>
2021-05-26 13:27:53 +0900

    Change ipnumber

--- testSub.py
+++ testSub.py
@@ -4,7 +4,7 @@ import PIL.Image as pilimg
 from PIL import Image
 import datetime
 
-publish.single("mydata/whoareyou/request", "videostreaming", hostname="ec2-52-78-81-16.ap-northeast-2.compute.amazonaws.com")
+#publish.single("mydata/whoareyou/request", "videostreaming", hostname="192.168.0.55")
 
 
 # on_connect는 subscriber가 브로커에 연결하면서 호출할 함수, rc가 0이면 정상 연결이 됐다는 의미
@@ -19,6 +19,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     now = datetime.datetime.now()
     filename = '%s.jpg' % now
+    filename = "".join(i for i in filename if i not in "\/:*?<>|")
     f = open(filename, "wb");
     f.write(msg.payload);
     f.close();
@@ -32,5 +33,5 @@ def on_message(client, userdata, msg):
 mqttClient = mqtt.Client();
 mqttClient.on_connect = on_connect; # 브로커에 연결이 되면 on_connect라는 함수가 실행되도록 등록 (핸들러함수(콜백) 등록)
 mqttClient.on_message = on_message; # 브로커에서 메시지가 전달되면 내가 등록해놓은 on_message함수가 실행
-mqttClient.connect("ec2-52-78-81-16.ap-northeast-2.compute.amazonaws.com", 1883, 60); # 브로커에 연결하기 # 연결 # keepalive : 브로커와 통신할 최대 시간
+mqttClient.connect("192.168.0.55", 1883, 60); # 브로커에 연결하기 # 연결 # keepalive : 브로커와 통신할 최대 시간
 mqttClient.loop_forever(); # 토픽이 전달될 때 까지 수신 대기
